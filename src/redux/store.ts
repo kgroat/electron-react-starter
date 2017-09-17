069ed6ff203686f88915b8a6c9136787e6813da7
Kevin Groat <apps@kevingroat.io>
2017-09-17 17:50:03 -0400

    Added redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,14 @@
 
 import { createStore } from 'redux'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  autoRehydrate()
+)
+
+persistStore(store)
 
 export default store
