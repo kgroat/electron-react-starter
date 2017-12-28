8398663da61044095b2e2b4aa42428d8b697c516
Kevin Groat <apps@kevingroat.io>
2017-12-28 14:39:05 -0500

    Adding Linux support (#14)
    * Beginning support for linux
    
    * hacktastic solution
    
    * Adding linux32
    
    * zipping linux builds

---
+++ scripts/generateOutputs/platforms/linux32.js
@@ -0,0 +1,56 @@
+
+const path = require('path')
+const fs = require('fs')
+
+const createDir = require('../createDir')
+const copyFile = require('../copyFile')
+
+const archiveUrl = (version) => `https://github.com/electron/electron/releases/download/v${version}/electron-v${version}-linux-ia32.zip`
+
+module.exports = {
+  archiveUrl: archiveUrl,
+  prepareApp: prepareApp,
+}
+
+function prepareApp (outputDir, appName) {
+  const resourcesDir = path.join(outputDir, 'resources')
+  const destDir = path.join(resourcesDir, 'app')
+  
+  return Promise.all([
+    createDir(destDir),
+    copyPngIcons(resourcesDir)
+  ])
+  .then(() => destDir)
+}
+
+function wait (ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+function renameApp(outputDir, appName) {
+  return rename(outputDir, {
+    source: 'electron',
+    dest: `${appName}`
+  })
+}
+
+function rename(outputDir, item) {
+  const source = path.join(outputDir, item.source)
+  const dest = path.join(outputDir, item.dest)
+
+  return new Promise((resolve, reject) => {
+    fs.rename(source, dest, (err) => {
+      if (err) { reject(err) }
+      else { resolve(dest) }
+    })
+  })
+}
+
+function copyPngIcons(resourcesDir) {
+  return copyFile(
+    path.join(__dirname, '../../../intermediates/icons/png'),
+    path.join(resourcesDir, 'icons')
+  )
+}
